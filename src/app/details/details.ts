b7a02b5308692c95c9ccc85adbb995e1b2c0ffd9
Cazolrac <c.caz.cg@gmail.com>
2025-11-23 12:56:01 -0600

    all porject

---
+++ src/app/details/details.ts
@@ -0,0 +1,20 @@
+import { NONE_TYPE } from '@angular/compiler';
+import { Component } from '@angular/core';
+import { ActivatedRoute, Route } from '@angular/router';
+import { PrerenderFallback } from '@angular/ssr';
+
+@Component({
+  selector: 'app-details',
+  standalone: false,
+  templateUrl: './details.html',
+  styleUrl: './details.css',
+})
+export class Details {
+  id: number | null = null;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.id = +this.route.snapshot.paramMap.get('id')!;
+  }
+}
